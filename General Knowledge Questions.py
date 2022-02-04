a482f75e8af09278d6a5a4c598ca05f9aa73dea6
mad4art <65566489+mad4art@users.noreply.github.com>
2022-02-04 21:14:23 +0000

    Add files via upload

---
+++ General Knowledge Questions.py
@@ -0,0 +1,102 @@
+print("Welcome to my general knowledge quiz, make sure to spell everything correctly.")
+print("This quiz in not timed, there are only 20 questions and answers will be at the end. Enjoy!")
+enter = input()
+print("What colour are the seats in the House of Commons?")
+answer1 = input()
+print("What year was Heinz established?")
+answer2 = input()
+print("What film was the first to be recognised as part of the Marvel Cinematic Universe?")
+answer3 = input()
+print("What colour is found on 75% of the world's flags?")
+answer4 = input()
+print("What is the name of Wendy’s dog in Peter Pan?")
+answer5 = input()
+print("What is the first name of Boris Johnson's newborn son?")
+answer6 = input()
+print("What is the most spoken language in the world?")
+answer7 = input()
+print("What is the name of the headteacher in Roald Dahl's Matilda?")
+answer8 = input()
+print("What colour is a giraffe's tongue?")
+answer9 = input()
+print("In the film 'The Lion King', what kind of animal is Timon?")
+answer10 = input()
+print("In what year was the Chernobyl disaster?")
+answer11 = input()
+print("How many legs does a lobster have?")
+answer12 = input()
+print("What do you call a group of jellyfish?")
+answer13 = input()
+print("What's the best selling book of all time?")
+answer14 = input()
+print("Which Queen of England had the name 'Bloody Mary'?")
+answer15 = input()
+print("Who is the voice of Shrek?")
+answer16 = input()
+print("How many teeth does an adult human have?")
+answer17 = input()
+print("What is the proper name for your funny bone?")
+answer18 = input()
+print("In what year was the first Playstation console released?")
+answer19 = input()
+print("Last Question!")
+print("What is the strongest sense in humans?")
+answer20 = input()
+if answer1 == "Green" or "green" or "gREEN" or "GREEN":
+    corrorno1 = "Correct."
+else:
+    corrorno1 = "Incorrect."
+if answer2 == "1869":
+    corrorno2 = "Correct."
+else:
+    corrorno2 = "Incorrect."
+if answer3 == "Iron Man" or "iron man" or "Iron man" or "iron Man" or "iRON MAN" or "iRON mAN" or "Iron Man":
+    corrorno3 = "Correct."
+else:
+    corrorno3 = "Incorrect."
+if answer4 == "Red" or "red" or "rED" or "RED":
+    corrorno4 = "Correct."
+else:
+    corrorno4 = "Incorrect."
+if answer5 == "Nana" or "nANA" or "nana" or "NANA":
+    corrorno5 = "Correct."
+else:
+    corrorno5 = "Incorrect."
+if answer6 == "Wilfred" or "wilfred" or "WILFRED" or "wILFRED":
+    corrorno6 = "Correct."
+else:
+    corrorno6 = "Incorrect."
+if answer7 == "Mandarin" or "mandarin" or "MANDARIN" or "mANDARIN":
+    corrorno7 = "Correct."
+else:
+    corrorno7 = "Incorrect."
+if answer8 == "Miss Trunchbull" or "Miss trunchbull" or "miss trunchbull" or "MISS TRUNCHBULL" or "mISS tRUNCHBULL":
+    corrorno8 = "Correct."
+else:
+    corrorno8 = "Incorrect."
+if answer9 == "Blue" or "blue" or "BLUE" or "bLUE":
+    corrorno9 = "Correct."
+else:
+    corrorno9 = "Incorrect."
+print("Ok, here are the answers.")
+print("Question 1. You said", answer1 ,"and the correct answer was Green.", corrorno1)
+print("Question 2. You said", answer2 ,"and the correct answer was 1869.", corrorno2)
+print("Question 3. You said", answer3 ,"and the correct answer was Iron Man.", corrorno3)
+print("Question 4. You said", answer4 ,"and the correct answer was Red.", corrorno4)
+print("Question 5. You said", answer5 ,"and the correct answer was Nana.", corrorno5)
+print("Question 6. You said", answer6 ,"and the correct answer was Wilfred.", corrorno6)
+print("Question 7. You said", answer7 ,"and the correct answer was Mandarin.", corrorno7)
+print("Question 8. You said", answer8 ,"and the correct answer was Miss Trunchbull.", corrorno8)
+print("Question 9. You said", answer9 ,"and the correct answer was Blue", corrorno9)
+print("Question 10. You said", answer10 ,"and the correct answer was Meerkat", corrorno10)
+print("Question 11. You said", answer11 ,"and the correct answer was 1986", corrorno11)
+print("Question 12. You said", answer12 ,"and the correct answer was Eight", corrorno12)
+print("Question 13. You said", answer13 ,"and the correct answer was a Bloom", corrorno13)
+print("Question 14. You said", answer14 ,"and the correct answer was The Bible", corrorno14)
+print("Question 15. You said", answer15 ,"and the correct answer was Mary I", corrorno15)
+print("Question 16. You said", answer16 ,"and the correct answer was Mike Myers", corrorno16)
+print("Question 17. You said", answer17 ,"and the correct answer was 32", corrorno17)
+print("Question 18. You said", answer18 ,"and the correct answer was Ulnar nerve", corrorno18)
+print("Question 19. You said", answer19 ,"and the correct answer was 1994", corrorno19)
+print("Question 20. You said", answer20 ,"and the correct answer was Smell", corrorno20)
+
